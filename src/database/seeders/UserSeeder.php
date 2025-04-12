210ecf40adf91f20693799c2d24816d0608b6348
nakatani <nakatani18.work@gmail.com>
2025-04-12 14:24:44 +0900

    breezeで認証機能の作成と日本語化

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+          [
+              'name' => '中谷',
+              'email' => 'tada950918@gmail.com',
+              'password' => Hash::make('password'),
+          ],
+      ]);
+    }
+}
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
